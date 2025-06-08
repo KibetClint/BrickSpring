274835ac04ef0dd575b9ff90d3ffca7e1ec9c9ac
KibetClint <clintonkibet98@gmail.com>
2025-06-08 19:17:14 +0300

    ...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -105,7 +105,7 @@ const Home = () => {
           </div>
           <div className="max-w-7xl mx-auto px-4">
             <div className="flex flex-wrap justify-center gap-2 mb-8">
-              {["All", "Hardware", "Software", "Services"].map((cat) => (
+              {["Hardware", "Software", "Services"].map((cat) => (
                 <button
                   key={cat}
                   onClick={() => setActiveFilter(cat)}
